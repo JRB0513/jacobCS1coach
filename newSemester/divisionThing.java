af83c10ec6b4bbe88cdd4c77767ff88885edc843
JRB0513 <100316904+JRB0513@users.noreply.github.com>
2024-01-10 16:51:50 +0000

    We're back!

---
+++ newSemester/divisionThing.java
@@ -0,0 +1,12 @@
+//Jacob Bradley
+class divisionThing
+{
+    public static void main(String args[])
+    {
+        int remain = (85 % 9);
+        int quote = (85 / 9);
+        {
+            System.out.print("If we divide 85 by 9, the remainder is " + remain + " and the quotient is " + quote );
+        }
+    }
+}
---
+++ newSemester/squareHalf.java
@@ -0,0 +1,13 @@
+// Jacob Bradley
+class squareHalf
+{
+    public static void main(String args[])
+    {
+        int num = 16;
+        int square = (num * num);
+        int fin = (square / 2);
+        {
+            System.out.print("If we take the number 16, square it, and then divide it by half, we get " + fin );
+        }
+    }
+}
