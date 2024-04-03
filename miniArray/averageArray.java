bb8c652d4d849ea97b2c693e3241ad2b72328315
JRB0513 <100316904+JRB0513@users.noreply.github.com>
2024-04-03 15:16:10 +0000

    Finally done

--- miniArray/averageArray.java
+++ miniArray/averageArray.java
@@ -5,21 +5,22 @@ class arraySum
     public static void main(String args[])
     {
         Scanner sel = new Scanner(System.in);
-        int [] nums = new int [5];
-        int tog = nums.length - 1;
-        int fin = 0;
-        int don = 0;
-        int al = 0;
+        double [] nums = new double [5];
+        int tog = nums.length;
+        double fin = 0;
+        double don = 0;
+        double al = 0;
         System.out.println("Enter 5 numbers: ");
         for (int i = 0; i < nums.length; i++){
             System.out.println("Enter number " + (i + 1) + ": ");
-            nums[i] = sel.nextInt();
+            nums[i] = sel.nextDouble();
         }
         for (int in = 0; in <= nums.length - 1; in++){
-            fin = nums[tog - in];
+            fin = nums[tog - in - 1];
             don = (don + fin);
-            al = (don / tog);
+            System.out.println(don);
         }
+        al = (don / tog);
         System.out.println(al);
     }
 }
